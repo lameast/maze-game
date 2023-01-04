664021cf4aaf30ae799c871e415eec1bbd607c03
Alan Wu <alanjialongwu@gmail.com>
2023-01-05 00:54:48 +1100

    Fixed maze generator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 
 import './App.css';
+import mazeCreator from './mazeCreator';
 
 function App() {
+
+  const maze = mazeCreator(15);
+
+  let cells = maze.map((row, i) => {
+    return row.map((cell, j) => {
+      return cell === 0 ? <div key={`${i}${j}`} className='wall'
+      style={{position: "absolute", top: `${5*j}px`, left: `${5*i}px`}}></div> : 
+      <div key={`${i}${j}`} style={{position: "absolute", top: `${5*j}px`, left: `${5*i}px`}}></div>;
+    });
+  })
+
+
   return (
     <div className="App">
+      {cells}
     </div>
   );
 }
--- src/mazeCreator.js
+++ src/mazeCreator.js
@@ -1,37 +1,49 @@
 const shuffleArray = (array) => {
     //Randomly shuffle array
     //Source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    const arr = array.slice(0);
-
-    for (let i = arr.length - 1; i > 0; i-- ){
-        const j = Math.floor(Math.random()*(i+1));
-        [arr[i], arr[j]] = [arr[j], [arr[i]]]
-    }
-
+    const arr = array.slice(0).map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
     return arr;
 }
 
 const mazeCreator = (size) => {
     //Iterative randomized DFS from https://en.wikipedia.org/wiki/Maze_generation_algorithm
     const mazeSize = size*3;
-    const maze = new Array(mazeSize).fill(new Array(mazeSize).fill(0));
+    //const maze = new Array(mazeSize).fill().map(() => new Array(mazeSize).fill(0));
+    const maze = new Array(mazeSize).fill().map(() => new Array(mazeSize).fill(0));
     const stack = [[1,1]];
     const directions = [[-2, 0], [0, -2], [2, 0], [0, 2]];
-
+    maze[1][1] = 1;
+    console.log(maze)
     while (stack.length > 0){
         let currentPosition = stack.pop();
-
-        for(const dir of shuffleArray(directions)){
-
-            let x = currentPosition[0] + dir[0];
-            let y = currentPosition[1] + dir[1];
-
-            if (0 < x < mazeSize-1 && 0 < y < mazeSize-1){
+        const shuffledArray = shuffleArray(directions);
+        for(let i = 0; i < shuffledArray.length; i++){
+            let dirX = shuffledArray[i][0];
+            let dirY = shuffledArray[i][1]
+            let x = currentPosition[0] + dirX;
+            let y = currentPosition[1] + dirY;
+            
+            if (x > 0 && x < mazeSize-1 && y > 0 && y < mazeSize-1 && maze[x][y] === 0){
+                
                 stack.push(currentPosition);
                 maze[x][y] = 1;
+                console.log(x,y);
+                if(dirX > 0){
+                    maze[x - 1][y] = 1;
+                }else if (dirX < 0){
+                    maze[x + 1][y] = 1;
+                }
+                else if (dirY > 0){
+                    maze[x][y - 1] = 1;
+                }
+                else if (dirY < 0){
+                    maze[x][y + 1] = 1;
+                }
                 stack.push([x,y]);
                 break;
-            }
+            } 
         }
     } 
     return maze;

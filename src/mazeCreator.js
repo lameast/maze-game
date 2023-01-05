f0bb4726c2b224fa4551fb1fb7cb6fab5d591e8a
Alan Wu <alanjialongwu@gmail.com>
2023-01-05 21:17:51 +1100

    Updated comments on maze generator

--- src/mazeCreator.js
+++ src/mazeCreator.js
@@ -10,26 +10,26 @@ const shuffleArray = (array) => {
 const mazeCreator = (size) => {
     //Iterative randomized DFS from https://en.wikipedia.org/wiki/Maze_generation_algorithm
     const mazeSize = size*3;
-    //const maze = new Array(mazeSize).fill().map(() => new Array(mazeSize).fill(0));
-    const maze = new Array(mazeSize).fill().map(() => new Array(mazeSize).fill(0));
+    const maze = new Array(mazeSize).fill().map(() => new Array(mazeSize).fill(0)); //Initialise maze
     const stack = [[1,1]];
-    const directions = [[-2, 0], [0, -2], [2, 0], [0, 2]];
+    const directions = [[-2, 0], [0, -2], [2, 0], [0, 2]]; //left, down, right, up
     maze[1][1] = 1;
-    console.log(maze)
+
     while (stack.length > 0){
         let currentPosition = stack.pop();
         const shuffledArray = shuffleArray(directions);
+        //Check cell in random direction
         for(let i = 0; i < shuffledArray.length; i++){
+            
             let dirX = shuffledArray[i][0];
             let dirY = shuffledArray[i][1]
             let x = currentPosition[0] + dirX;
             let y = currentPosition[1] + dirY;
             
             if (x > 0 && x < mazeSize-1 && y > 0 && y < mazeSize-1 && maze[x][y] === 0){
-                
+                //If unvisited, visit this cell
                 stack.push(currentPosition);
-                maze[x][y] = 1;
-                console.log(x,y);
+                maze[x][y] = 1; //Mark cell as visited
                 if(dirX > 0){
                     maze[x - 1][y] = 1;
                 }else if (dirX < 0){
